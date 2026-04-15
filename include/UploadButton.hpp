d359336e1ee8bfd32968a6e7034d6614c35e7b7d
Dorian Eydoux <bonjour@deydoux.fr>
2026-04-15 22:45:04 +0200

    Add UploadButton::openFile

--- include/UploadButton.hpp
+++ include/UploadButton.hpp
@@ -1,12 +1,21 @@
 #ifndef __UPLOADBUTTON_HPP__
 # define __UPLOADBUTTON_HPP__
 
+#include <QException>
+#include <QFile>
 #include <QPushButton>
 
 class UploadButton: public QPushButton
 {
 public:
-	UploadButton(const QString &text = "Upload Map...");
+	UploadButton();
+
+	std::shared_ptr<QFile> openFile(const QString &path);
+
+private:
+	class OpenFail: public QException {
+		void raise() const override { throw *this; }
+	};
 };
 
 #endif /* __UPLOADBUTTON_HPP__ */
--- src/UploadButton.cpp
+++ src/UploadButton.cpp
@@ -2,11 +2,25 @@
 #include <QDebug>
 #include <QFileDialog>
 
-UploadButton::UploadButton(const QString &text): QPushButton(text)
+UploadButton::UploadButton(): QPushButton("Upload Map...")
 {
-	connect(this, &QAbstractButton::pressed, []() {
-		const QString fileName = QFileDialog::getOpenFileName();
-		qDebug() << "Opened:" << fileName;
+	connect(this, &QAbstractButton::pressed, [this]() {
+		const QString path = QFileDialog::getOpenFileName();
+		try {
+			openFile(path);
+		} catch (OpenFail &e) {
 
+		}
 	});
+}
+
+std::shared_ptr<QFile> UploadButton::openFile(const QString &path)
+{
+	auto file = std::make_shared<QFile>(path);
+
+	if (!file->open(QIODevice::ReadOnly))
+		throw new OpenFail();
+
+	qDebug() << "Opened:" << file->fileName();
+	return file;
 }
